f206aed62cda09d1c829444ebf27d73a0d56db23
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2023-01-01 09:29:53 +0900

    INIT

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+dotenv.config();
+const indexRouter = require('./routes');
+const app = express();
+app.set('port', process.env.PORT || 8005);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
+
+app.use('/', indexRouter);
+
+// 위에 경로(path) 가 없는 경우 마지막 함수. 404
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status(404);
+    next(error);
+});
+
+// 마지막 에러 처리 
+app.use((err, req, res, next) => {
+   res.locals.message = err.message;
+   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+   res.status(err.status || 500);
+   res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.get('port')} 포트로 서비스가 시작되었습니다.`);
+})
