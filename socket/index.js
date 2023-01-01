c7dd93c19215aea24d5f3d52c006a75a5eb02e40
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2023-01-01 11:14:39 +0900

    we 웹 소켓 (Socket.io)

--- socket/index.js
+++ socket/index.js
@@ -1,30 +1,28 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+    const io = SocketIO(server, { path: '/socket.io'});
 
-    wss.on('connection', (ws, req) => {
-       const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-       console.log('새로운 클라이언트 접속', ip);
-       ws.on('message', (message) => {
-           console.log(message.toString());
-       });
+    io.on('connection', (socket) => {
+        const req = socket.request;
+        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        // socket.id 연결에 아이디
+        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+        socket.on('disconnect', () => {
+            console.log('클라이언트 접속 해제', ip, socket.id);
 
-       ws.on('error', console.error);
-       ws.on('close', () => {
-           console.log('클라이언트 접속 해제', ip);
+            // 연결이 끊어진 경우 인터벌을 멈추어야 한다.
+            clearInterval(socket.interval);
+        });
+        socket.on('reply', (data) => {
+            console.log(data);
+        })
 
-           // 연결이 끊어진 경우 인터벌을 멈추어야 한다.
-           clearInterval(ws.interval);
-       });
-
-       ws.interval = setInterval(() => {
-           // 웹소켓 연결을 맺고 바로 전송하는 경우 안되는 경우가 있다.
-
-           if (ws.readyState === ws.OPEN) {
-               ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-           }
-       }, 3000);
+        socket.on('error', console.error);
+        socket.interval = setInterval(() => {
+            // 웹소켓 연결을 맺고 바로 전송하는 경우 안되는 경우가 있다.
+            socket.emit('news', 'Hello Socket.IO');
+        }, 3000);
 
     });
 };
